b9c4ca03b21aacc7a300a6397b1db3523a370aa0
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-09-27 21:18:18 -0500

    update ProfileUpdate screen

--- app/src/main/java/pe/edu/hiresync_mobileapp/ui/profile/Profile.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/ui/profile/Profile.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,11 +19,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import pe.edu.hiresync_mobileapp.R
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun Profile(){
+    var receiveNews by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -61,6 +68,49 @@ fun Profile(){
             color = Color(0xFF6650a4),
             modifier = Modifier.padding(top = 8.dp)
         )
+        ProfileUpdateCard()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = receiveNews,
+                onCheckedChange = { isChecked ->
+                    receiveNews = isChecked
+                },
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(
+                text = "I want to receive news from HireSync via email",
+                fontSize = 16.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = { /* Handle Profile Settings click */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            ) {
+                Text(text = "Profile Settings")
+            }
+            Button(
+                onClick = { /* Handle App Settings click */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp)
+            ) {
+                Text(text = "App Settings")
+            }
+        }
     }
 }
 
