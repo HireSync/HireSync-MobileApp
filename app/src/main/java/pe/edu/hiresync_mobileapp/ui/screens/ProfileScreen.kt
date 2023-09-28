6ec2b1b8e11746d16204dcd81238581cdd418988
FabriHM <u202118067@upc.edu.pe>
2023-09-27 23:30:18 -0500

    feat: added ProfileScreen

---
+++ app/src/main/java/pe/edu/hiresync_mobileapp/ui/screens/ProfileScreen.kt
@@ -0,0 +1,252 @@
+package pe.edu.hiresync_mobileapp.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import pe.edu.hiresync_mobileapp.R
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import pe.edu.hiresync_mobileapp.ui.viewModel.LoginViewModel
+
+@Composable
+fun ProfileScreen(navController: NavController, viewModel: LoginViewModel){
+    var receiveNews by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Blue)
+
+        ) {
+            NavBar(navController)
+        }
+        Image(
+            painter = painterResource(id = R.drawable.profilepicture),
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .wrapContentWidth()
+                .background(
+                    Color(0xFF6650a4),
+                    shape = CircleShape
+                ),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White
+            ),
+        ) {
+            Text(text = "Change profile picture")
+        }
+        Text(
+            text = "Natalia Gomez",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF6650a4),
+            modifier = Modifier.padding(top = 8.dp)
+        )
+        ProfileUpdateCard()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = receiveNews,
+                onCheckedChange = { isChecked ->
+                    receiveNews = isChecked
+                },
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(
+                text = "I want to receive news from HireSync via email",
+                fontSize = 16.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = { /* Handle Profile Settings click */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            ) {
+                Text(text = "Profile Settings")
+            }
+            Button(
+                onClick = { /* Handle App Settings click */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp)
+            ) {
+                Text(text = "App Settings")
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileUpdateCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+            .background(Color.White)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = "Name:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+                Text(
+                    text = "Email:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+
+                Text(
+                    text = "Confirm Password:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = "Current Password:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+                Text(
+                    text = "New Password:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+                Text(
+                    text = "Phone Number:",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+                BasicTextField(
+                    value = "",
+                    onValueChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                        .border(1.dp, Color.Black)
+                )
+            }
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .background(Color(0xFF6650a4), shape = CircleShape)
+        ) {
+            Text(
+                text = "Save Changes",
+                color = Color.White
+            )
+        }
+    }
+}
