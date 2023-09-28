51c904d64facf2a85b7ca1a2492fca765364bbe4
FabriHM <u202118067@upc.edu.pe>
2023-09-27 22:36:48 -0500

    chore: deleted never used

--- app/src/main/java/pe/edu/hiresync_mobileapp/MainActivity.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/MainActivity.kt
@@ -4,21 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import pe.edu.hiresync_mobileapp.ui.home.Home
 import pe.edu.hiresync_mobileapp.ui.navigation.AppNavigation
 import pe.edu.hiresync_mobileapp.ui.theme.HireSyncMobileAppTheme
 import pe.edu.hiresync_mobileapp.ui.viewModel.LoginViewModel
@@ -39,18 +30,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
