5ff1ff642cf529c28fae75f650a3e9839bdf1084
Anderson Velasquez <andersonvelezca@gmail.com>
2023-10-29 18:49:00 -0500

    feature: added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,14 +56,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.3")
     val navVersion = "2.7.2"
-
-    val retrofit = "2.9.0"
-    implementation("com.squareup.retrofit2:retrofit:$retrofit")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofit")
-
-
-
-
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -81,7 +73,31 @@ dependencies {
     implementation ("androidx.compose.material:material-icons-core")
     implementation ("androidx.compose.material:material-icons-extended")
 
+    implementation("androidx.compose.runtime:runtime-livedata")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    implementation("com.github.skydoves:landscapist-glide:2.1.9")
 
 
+    val coilVersion = "2.3.0"
+    implementation("io.coil-kt:coil-compose:$coilVersion")
 
+    val room_version = "2.5.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    val lifecycleVersion = "2.6.1"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${lifecycleVersion}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${lifecycleVersion}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${lifecycleVersion}")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${lifecycleVersion}")
+
+    val retrofitVersion= "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 }
