74c3d3d9a4d5aade916594b9a2309391b012f314
FabriHM <u202118067@upc.edu.pe>
2023-10-29 15:52:40 -0500

    feat: updated data class

--- app/src/main/java/pe/edu/hiresync_mobileapp/data/model/Postulation.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/data/model/Postulation.kt
@@ -3,19 +3,27 @@ package pe.edu.hiresync_mobileapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class PostulationResponse(
+    @SerializedName("results")
     val postulations: List<Postulation>
 )
 
 data class Postulation(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("description")
+    val position: String,
     val description: String,
-    @SerializedName("created_date")
-    val createdDate: String
+    val applicationDate: String,
+    val location: String,
+    val salary: String,
+    val contractType: String,
+    val requirements: String,
+    val contact: Contact,
+    var isApplied: Boolean = false
 )
 
+data class Contact(
+    val email: String,
+    val phone: String
+)
 
 
