a3f5739f7cb0f75f760be67bc82ad15ff2bfa5fd
FabriHM <u202118067@upc.edu.pe>
2023-10-29 03:15:54 -0500

    feat: updated data model

--- app/src/main/java/pe/edu/hiresync_mobileapp/data/model/Postulation.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/data/model/Postulation.kt
@@ -1,11 +1,14 @@
 package pe.edu.hiresync_mobileapp.data.model
 
 
+data class PostulationResponse(val postulations: List<Postulation>)
+
 data class Postulation(
-    val Id:Int,
-    val Name : String,
-    val Description: String,
-    val StartingDate: String,
-    val DateTime: String,
-    val Status: Boolean
+    val id: Int,
+    val name: String,
+    val description: String,
+    val startingDate: String,
+    val endingDate: String,
+    val status: Boolean
 )
+
