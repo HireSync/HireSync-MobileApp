cd1a85f3cffae1d49c249f56112f9a37d682f3cb
FabriHM <u202118067@upc.edu.pe>
2023-10-29 15:51:49 -0500

    feat: updated code and delete unused import

--- app/src/main/java/pe/edu/hiresync_mobileapp/ui/screens/PostulationsScreen.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/ui/screens/PostulationsScreen.kt
@@ -1,31 +1,24 @@
 package pe.edu.hiresync_mobileapp.ui.screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -33,45 +26,122 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.glide.GlideImage
 import pe.edu.hiresync_mobileapp.data.model.Postulation
-import pe.edu.hiresync_mobileapp.ui.viewModel.LoginViewModel
 import pe.edu.hiresync_mobileapp.ui.viewModel.PostulationViewModel
 
 @Composable
-fun PostulationsScreen(viewModel: PostulationViewModel) {
+fun PostulationsScreen(navController: NavController, viewModel: PostulationViewModel) {
 
     val postulations: List<Postulation> by viewModel.postulations.observeAsState(listOf())
     viewModel.getAll()
-    LazyColumn {
-        items(postulations) { postulation ->
-            CharacterCard(postulation)
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Blue)
+
+        ) {
+            NavBar(navController)
+        }
+        Column{
+            LazyColumn {
+                items(postulations) { postulation ->
+                    PostulationCard(postulation, navController)
+                }
+            }
         }
     }
+
 }
 
 @Composable
-fun CharacterCard(postulation: Postulation) {
-    Card(modifier = Modifier.padding(8.dp)) {
-        Row(
+fun PostulationCard(postulation: Postulation, navController: NavController) {
+    val isApplied = remember { mutableStateOf(false) }
+    isApplied.value = postulation.isApplied
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.White)
                 .padding(8.dp)
+                .background(Color.White)
+                .border(1.dp, Color.Gray, shape = RoundedCornerShape(8.dp))
+                //.clickable { }
+                .shadow(4.dp)
         ) {
-            Column(modifier = Modifier.weight(5f)) {
-                Text(text = "Name: "+postulation.name)
-                Text(text = "Description: "+postulation.id.toString())
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = postulation.name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                    if (isApplied.value) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "Applied",
+                            tint = Color.Green
+                        )
+                    }
+                }
+                Text(text = postulation.description, fontSize = 14.sp, color = Color.Gray)
+                Text(
+                    text = "Application Date: ${postulation.applicationDate}",
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+
+                // Detalles adicionales
+                Text(text = "Location: ${postulation.location}", fontSize = 12.sp, color = Color.Gray)
+                Text(text = "Salary: ${postulation.salary}", fontSize = 12.sp, color = Color.Gray)
+                Text(
+                    text = "Contract Type: ${postulation.contractType}",
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+                Row(modifier = Modifier.padding(top = 8.dp)) {
+                    Button(
+                        onClick = { },
+                        colors = ButtonDefaults.buttonColors(Color(0xFF3172D4))
+                    ) {
+                        Text(text = "View Details", color = Color.White) // Color del texto en blanco
+                    }
+
+                    TextButton(
+                        onClick = {
+                            if (!isApplied.value) {
+                                isApplied.value = true
+                            }
+                        },
+                        colors = ButtonDefaults.textButtonColors(contentColor = Color(0xFF3172D4))
+                    ) {
+                        Text(text = "Apply", color = Color(0xFF3172D4))
+                    }
+                }
             }
         }
     }
