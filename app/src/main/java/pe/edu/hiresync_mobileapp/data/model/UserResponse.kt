5a8a4012c2ee1d744ed9f27d6a441a9ea7538805
Fabricio Huillca <102436134+FabriHM@users.noreply.github.com>
2023-11-21 18:18:40 -0500

    Update UserResponse.kt

--- app/src/main/java/pe/edu/hiresync_mobileapp/data/model/UserResponse.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/data/model/UserResponse.kt
@@ -2,7 +2,7 @@ package pe.edu.hiresync_mobileapp.data.model
 
 data class UserResponse (
     val firstName : String,
-    val lastName : String ,
+    val password : String ,
     val email   : String ,
     val id : Int
 
