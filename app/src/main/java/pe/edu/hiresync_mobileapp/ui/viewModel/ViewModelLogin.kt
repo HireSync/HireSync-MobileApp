0363c3b9209d043183925a1c0f40b3551a5525fa
Fabricio Huillca <102436134+FabriHM@users.noreply.github.com>
2023-11-22 18:32:48 -0500

    feat: updated ViewModelLogin

--- app/src/main/java/pe/edu/hiresync_mobileapp/ui/viewModel/ViewModelLogin.kt
+++ app/src/main/java/pe/edu/hiresync_mobileapp/ui/viewModel/ViewModelLogin.kt
@@ -11,8 +11,18 @@ class ViewModelLogin(private val userRepository: UserRepository = UserRepository
     private val _email = MutableLiveData<String>()
     val email: LiveData<String> = _email
 
+    private val _loggedInUser = MutableLiveData<UserResponse?>()
+    val loggedInUser: LiveData<UserResponse?> get() = _loggedInUser
+    
     private val _password = MutableLiveData<String>()
     val password: LiveData<String> = _password
+
+    private val _name = MutableLiveData<String>()
+    val name: LiveData<String> get() = _name
+
+    private val _phone = MutableLiveData<String>()
+    val phone: LiveData<String> get() = _phone
+
 
     private val _isLoggedIn = MutableLiveData<Boolean>()
     val isLoggedIn: LiveData<Boolean> get() = _isLoggedIn
@@ -33,6 +43,18 @@ class ViewModelLogin(private val userRepository: UserRepository = UserRepository
                 val users = result.data
                 if(!users.isNullOrEmpty()){
                     _isLoggedIn.value = true
+                    val loggedInUser = users[0]
+                    _loggedInUser.value = loggedInUser
+                }
+            }
+        }
+    }
+
+    fun editProfile(updatedProfile: UserRequest) {
+        val userId = _loggedInUser.value?.id
+        if (userId != null) {
+            userRepository.editProfile(userId, updatedProfile) { result ->
+                if (result is Result.Success) {
                 }
             }
         }
